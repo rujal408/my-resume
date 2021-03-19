f4424894202849c25c628d72a3b33733c51a307c
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-19 16:01:51 +0545

    skill sorting

--- src/Components/NavComponents/MySkills/index.js
+++ src/Components/NavComponents/MySkills/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Skill from './Skill'
 
 const skills = [
-    { id: 1, title: "JAVASCRIPT/REACT", progress: 80 },
-    { id: 2, title: "HTML", progress: 80 },
-    { id: 3, title: "CSS", progress: 50 },
-    { id: 4, title: "PHP/LARAVEL", progress: 45 },
+    { id: 1, title: "HTML", progress: 75 },
+    { id: 2, title: "CSS", progress: 40 },
+    { id: 3, title: "JAVASCRIPT/REACT", progress: 80 },
+    { id: 4, title: "PHP/LARAVEL", progress: 40 },
     { id: 5, title: "PYTHON/DJANGO", progress: 30 }
 ]
 
@@ -18,7 +18,7 @@ function MySkills() {
             </div>
             <div className="line"></div>
             {
-                skills.map(x => (
+                skills.sort((a,b)=>b.progress - a.progress).map(x => (
                     <Skill key={x.id} {...x} />
                 ))
             }
